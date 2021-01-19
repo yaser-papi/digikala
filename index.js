95a59d1eeef17250c2208f4929c47070af02f41a
yaser-papi <73193877+yaser-papi@users.noreply.github.com>
2021-01-20 00:34:47 +0330

    add test api

--- index.js
+++ index.js
@@ -5,5 +5,16 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import axios from 'axios';
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log('response', response);
+    return response;
+  },
+  (error) => {
+    console.log('error', error);
+  },
+);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/screens/main.js
+++ src/screens/main.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import My_Header from '../component/header_page/header';
 import {ScrollView} from 'react-native';
 import Slider from '../component/main_page/slider';
@@ -8,23 +9,47 @@ import Off from '../component/main_page/off';
 import KalaOne from '../component/main_page/KalaOne';
 import KalaTwo from '../component/main_page/KalaTwo';
 import Amazing from '../component/main_page/amazing';
+import {Button, Text} from 'native-base';
 
 let props = {
   head_name: 'Main',
 };
 
 const Main = () => {
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.ir/posts')
+      .then((response) => console.log('response.data', response.data));
+    // .catch(error)(console.log('error', error));
+  });
+  const buttonHandler = async () => {
+    axios
+      .get('https://jsonplaceholder.ir/posts')
+      .then((response) => console.log('response.data', response.data))
+      .catch(error)(console.log('error', error));
+  };
+  // axios}
+
+  //   .get('https://jsonplaceholder.ir/posts/50')
+  //   .then((response) => {
+  //     console.log('response.data', response.data);
+  //   })
+  //   .catch((error) => console.log('error', error));
+
   return (
-    <ScrollView>
-      <My_Header {...props} />
-      <Slider />
-      <Category />
-      <Amazing />
-      <Offer />
-      <Off />
-      <KalaOne />
-      <KalaTwo />
-    </ScrollView>
+    // <ScrollView>
+    //   <My_Header {...props} />
+    //   <Slider />
+    //   <Category />
+    //   <Amazing />
+    //   <Offer />
+    //   <Off />
+    //   <KalaOne />
+    //   <KalaTwo />
+    // </ScrollView>
+    <Button onPress={buttonHandler}>
+      <Text>click</Text>
+    </Button>
   );
 };
 
